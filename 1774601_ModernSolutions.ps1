a3dc7b40a30f75c2221cf0ea2e0510d6681178a1
brookelynpalfy <60612194+brookelynpalfy@users.noreply.github.com>
2020-03-13 11:11:54 -0400

    Add files via upload

---
+++ 1774601_ModernSolutions.ps1
@@ -0,0 +1,40 @@
+﻿$processName="notepad"
+KillThatProcess $processName
+
+Bamboozle $path
+
+function KillThatProcess ([string]$processName) 
+{
+
+    $processes=(Get-Process $processName)
+    
+
+    $input=(Read-Host ("There are "+(@(Get-Process $processName).count)+" processes with the name "+$ProcessName+", proceed? Y/N")).ToUpper() 
+
+    if( $input -eq "Y")
+    {
+        $processes|kill
+        Write-Host -BackgroundColor Green -ForegroundColor Black "The processes have been stopped"
+    }
+    else
+    {
+        
+        Write-Host -BackgroundColor Red -ForegroundColor Yellow "Operation Cancelled"
+    }
+    
+}
+
+<#$path= Get-Location#>
+function Bamboozle ([string] $path="default")
+{
+    [char]$letter=((65..90) | Get-Random -count 1)
+
+    Write-Host -BackgroundColor black -ForegroundColor Red "Time to have some fun! All of your files containing letter: $letter will be destroyed. MUAHAHAHA"
+
+    $list=ls *$letter* -File
+    
+    $list|Remove-Item -WhatIf
+    
+}
+
+
